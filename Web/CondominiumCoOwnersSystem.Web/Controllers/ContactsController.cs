90220e706e024dfc558b113de8ec16b62839915d
Ivan Gaydarov <iwan.gaidarow@gmail.com>
2020-03-11 14:16:03 +0200

    Added Contact page and Contact Controller. Integrated ReCaptcha for contact form.

---
+++ Web/CondominiumCoOwnersSystem.Web/Controllers/ContactsController.cs
@@ -0,0 +1,29 @@
+﻿namespace CondominiumCoOwnersSystem.Web.Controllers
+{
+    using CondominiumCoOwnersSystem.Web.ViewModels.Contacts;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ContactsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(ContactFormViewModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            return this.RedirectToAction("ThankYou");
+        }
+
+        public IActionResult ThankYou()
+        {
+            return this.View();
+        }
+    }
+}
